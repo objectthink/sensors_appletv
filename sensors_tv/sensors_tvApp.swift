7c5c83131bdd10a163d942929b30317dc3853f78
stephen eshelman, objectthink <stephen.n.eshelman@objectthink.com>
2022-02-18 09:54:59 -0500

    Initial Commit

---
+++ sensors_tv/sensors_tvApp.swift
@@ -0,0 +1,17 @@
+//
+//  sensors_tvApp.swift
+//  sensors_tv
+//
+//  Created by stephen eshelman on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct sensors_tvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
